478d126f2983882a5bff0bea1b2530c470733496
matt-humphrey <34499789+matt-humphrey@users.noreply.github.com>
2021-03-17 22:01:47 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -256,11 +256,11 @@ class Detections:
         colors = color_list()
         str = ""
         for i, (img, pred) in enumerate(zip(self.imgs, self.pred)):
-            str += f'image {i + 1}/{len(self.pred)}: {img.shape[0]}x{img.shape[1]} ' + '\n'
+            str += f'image {i + 1}/{len(self.pred)}: {img.shape[0]}x{img.shape[1]} '
             if pred is not None:
                 for c in pred[:, -1].unique():
                     n = (pred[:, -1] == c).sum()  # detections per class
-                    str += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    str += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  + "\n"
                 if show or save or render:
                     for *box, conf, cls in pred:  # xyxy, confidence, class
                         label = f'{self.names[int(cls)]} {conf:.2f}'
