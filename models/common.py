d0c7c610785a37a62917278d2e76834aa773b659
matt-humphrey <34499789+matt-humphrey@users.noreply.github.com>
2021-03-23 16:19:29 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -257,12 +257,14 @@ class Detections:
         txt = ""
         for i, (img, pred) in enumerate(zip(self.imgs, self.pred)):
             str = f'image {i + 1}/{len(self.pred)}: {img.shape[0]}x{img.shape[1]}, ' 
-            txt += f'{pred}'
+            txt += f'{i}: '
 #             if pred is not None:
             for c in pred[:, -1].unique():
                 n = (pred[:, -1] == c).sum()  # detections per class
                 str += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  + "\n"
-                txt += f"{type(n)}" + "\n"
+                txt += f"{n}" + "\n"
+                print(f'c: {c})
+                print(f'n: {n})
             if show or save or render:
                 for *box, conf, cls in pred:  # xyxy, confidence, class
                     label = f'{self.names[int(cls)]} {conf:.2f}'
