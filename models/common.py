244173ea5e5284cbd6a56a25a4fb20280bec7faa
matt-humphrey <34499789+matt-humphrey@users.noreply.github.com>
2021-03-23 11:58:16 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -257,17 +257,21 @@ class Detections:
         txt = ""
         for i, (img, pred) in enumerate(zip(self.imgs, self.pred)):
             str = f'image {i + 1}/{len(self.pred)}: {img.shape[0]}x{img.shape[1]}, '
+            count = 0
             if pred is not None:
                 for c in pred[:, -1].unique():
                     n = (pred[:, -1] == c).sum()  # detections per class
                     str += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  + "\n"
-                    txt += f'{i}: {n}' + "\n"
+                    if i == count:
+                        txt += f'{i}: {n}' + "\n"
+                        count += 1
+                    else:
+                        txt += f'{count}: 0' + "\n"
+                        count += 1        
                 if show or save or render:
                     for *box, conf, cls in pred:  # xyxy, confidence, class
                         label = f'{self.names[int(cls)]} {conf:.2f}'
                         plot_one_box(box, img, label=label, color=colors[int(cls) % 10])
-            elif pred is None:
-                txt += f'{i}: 0' + "\n"
             img = Image.fromarray(img.astype(np.uint8)) if isinstance(img, np.ndarray) else img  # from np
             if pprint:
                 print(str.rstrip(', '))
