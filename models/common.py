8761698127b0e0435ca87b4bf8667bf270908223
matt-humphrey <34499789+matt-humphrey@users.noreply.github.com>
2021-03-23 16:38:19 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -255,7 +255,7 @@ class Detections:
     def display(self, pprint=False, show=False, save=False, render=False, save_txt=False, save_dir=''):
         colors = color_list()
         txt = ""
-        count = 0
+        count = -1
         for i, (img, pred) in enumerate(zip(self.imgs, self.pred)):
             str = f'image {i + 1}/{len(self.pred)}: {img.shape[0]}x{img.shape[1]}, ' 
             txt += f'{i}: '
@@ -263,11 +263,12 @@ class Detections:
                 for c in pred[:, -1].unique():
                     n = (pred[:, -1] == c).sum()  # detections per class
                     str += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  + "\n"
+                    count += 1
+                    print(count, i)
                     if count == i:
                         txt += f"{n}" + "\n"
                     else:
                         txt += "0" + "\n"
-                    count += 1
             if show or save or render:
                 for *box, conf, cls in pred:  # xyxy, confidence, class
                     label = f'{self.names[int(cls)]} {conf:.2f}'
