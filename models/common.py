f2e2f8a2425ac803df2d936731f26e907f38b9e9
matt-humphrey <34499789+matt-humphrey@users.noreply.github.com>
2021-03-23 16:29:07 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -255,16 +255,19 @@ class Detections:
     def display(self, pprint=False, show=False, save=False, render=False, save_txt=False, save_dir=''):
         colors = color_list()
         txt = ""
+        count = 0
         for i, (img, pred) in enumerate(zip(self.imgs, self.pred)):
             str = f'image {i + 1}/{len(self.pred)}: {img.shape[0]}x{img.shape[1]}, ' 
             txt += f'{i}: '
-#             if pred is not None:
-            for c in pred[:, -1].unique():
-                n = (pred[:, -1] == c).sum()  # detections per class
-                str += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  + "\n"
-                txt += f"{n}" + "\n"
-                print(f'c: {c}')
-                print(f'n: {n}')
+            if pred is not None:
+                for c in pred[:, -1].unique():
+                    n = (pred[:, -1] == c).sum()  # detections per class
+                    str += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  + "\n"
+                    if count == i:
+                        txt += f"{n}" + "\n"
+                    else:
+                        txt += "0" + "\n"
+                    count += 1
             if show or save or render:
                 for *box, conf, cls in pred:  # xyxy, confidence, class
                     label = f'{self.names[int(cls)]} {conf:.2f}'
